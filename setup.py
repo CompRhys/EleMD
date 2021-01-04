0b57a1e0f8826895074d9bd628bb54a25e38233e
Rhys Goodall <reag2@cam.ac.uk>
2021-01-04 11:25:51 +0000

    fix: attempt to get ci to run correctly

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="elemd",
+    name="EleMD",
     version="0.0.1",
     author="Rhys Goodall",
     author_email="reag2@cam.ac.uk",
