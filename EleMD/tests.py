75f6ecc18dc7cb9a3533aa3fa5d720ca0b40f748
Rhys Goodall <reag2@cam.ac.uk>
2021-07-13 16:07:03 +0100

    test: reduce tests tol

--- EleMD/tests.py
+++ EleMD/tests.py
@@ -2,6 +2,7 @@ import math
 from EleMD import EleMD
 from ElMD import ElMD
 
+TEST_TOL = 1e-4
 
 def test_against_reference():
     """
@@ -9,27 +10,27 @@ def test_against_reference():
     """
     elemd = EleMD("atomic").elemd
     elmd = ElMD(metric="atomic").elmd
-    assert math.isclose(elemd("Zr3AlN", "CaTiO3"), elmd("Zr3AlN", "CaTiO3"), abs_tol=1e-5)
-    assert math.isclose(elemd("Li7La3Hf2O12", "CsPbI3"), elmd("Li7La3Hf2O12", "CsPbI3"), abs_tol=1e-5)
-    assert math.isclose(elemd("Zr3AlN", "CsPbI3"), elmd("Zr3AlN", "CsPbI3"), abs_tol=1e-5)
+    assert math.isclose(elemd("Zr3AlN", "CaTiO3"), elmd("Zr3AlN", "CaTiO3"), abs_tol=TEST_TOL)
+    assert math.isclose(elemd("Li7La3Hf2O12", "CsPbI3"), elmd("Li7La3Hf2O12", "CsPbI3"), abs_tol=TEST_TOL)
+    assert math.isclose(elemd("Zr3AlN", "CsPbI3"), elmd("Zr3AlN", "CsPbI3"), abs_tol=TEST_TOL)
 
     elemd = EleMD("mendeleev").elemd
     elmd = ElMD(metric="mendeleev").elmd
-    assert math.isclose(elemd("Zr3AlN", "CaTiO3"), elmd("Zr3AlN", "CaTiO3"), abs_tol=1e-5)
-    assert math.isclose(elemd("Li7La3Hf2O12", "CsPbI3"), elmd("Li7La3Hf2O12", "CsPbI3"), abs_tol=1e-5)
-    assert math.isclose(elemd("Zr3AlN", "CsPbI3"), elmd("Zr3AlN", "CsPbI3"), abs_tol=1e-5)
+    assert math.isclose(elemd("Zr3AlN", "CaTiO3"), elmd("Zr3AlN", "CaTiO3"), abs_tol=TEST_TOL)
+    assert math.isclose(elemd("Li7La3Hf2O12", "CsPbI3"), elmd("Li7La3Hf2O12", "CsPbI3"), abs_tol=TEST_TOL)
+    assert math.isclose(elemd("Zr3AlN", "CsPbI3"), elmd("Zr3AlN", "CsPbI3"), abs_tol=TEST_TOL)
 
     elemd = EleMD("pettifor").elemd
     elmd = ElMD(metric="petti").elmd
-    assert math.isclose(elemd("Zr3AlN", "CaTiO3"), elmd("Zr3AlN", "CaTiO3"), abs_tol=1e-5)
-    assert math.isclose(elemd("Li7La3Hf2O12", "CsPbI3"), elmd("Li7La3Hf2O12", "CsPbI3"), abs_tol=1e-5)
-    assert math.isclose(elemd("Zr3AlN", "CsPbI3"), elmd("Zr3AlN", "CsPbI3"), abs_tol=1e-5)
+    assert math.isclose(elemd("Zr3AlN", "CaTiO3"), elmd("Zr3AlN", "CaTiO3"), abs_tol=TEST_TOL)
+    assert math.isclose(elemd("Li7La3Hf2O12", "CsPbI3"), elmd("Li7La3Hf2O12", "CsPbI3"), abs_tol=TEST_TOL)
+    assert math.isclose(elemd("Zr3AlN", "CsPbI3"), elmd("Zr3AlN", "CsPbI3"), abs_tol=TEST_TOL)
 
     elemd = EleMD("mod_pettifor").elemd
     elmd = ElMD(metric="mod_petti").elmd
-    assert math.isclose(elemd("Zr3AlN", "CaTiO3"), elmd("Zr3AlN", "CaTiO3"), abs_tol=1e-5)
-    assert math.isclose(elemd("Li7La3Hf2O12", "CsPbI3"), elmd("Li7La3Hf2O12", "CsPbI3"), abs_tol=1e-5)
-    assert math.isclose(elemd("Zr3AlN", "CsPbI3"), elmd("Zr3AlN", "CsPbI3"), abs_tol=1e-5)
+    assert math.isclose(elemd("Zr3AlN", "CaTiO3"), elmd("Zr3AlN", "CaTiO3"), abs_tol=TEST_TOL)
+    assert math.isclose(elemd("Li7La3Hf2O12", "CsPbI3"), elmd("Li7La3Hf2O12", "CsPbI3"), abs_tol=TEST_TOL)
+    assert math.isclose(elemd("Zr3AlN", "CsPbI3"), elmd("Zr3AlN", "CsPbI3"), abs_tol=TEST_TOL)
 
 
 if __name__ == "__main__":
